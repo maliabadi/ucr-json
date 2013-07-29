a50da57ffc34e6024b62bbf32c9a14b46aa3a1c6
matt aliabadi <mattmaliabadi@gmail.com>
2013-07-29 01:16:40 -0700

    all the yearly data normalized

--- scripts/yearly_data.rb
+++ scripts/yearly_data.rb
@@ -46,7 +46,33 @@ def run
 	end
 end
 
+def one study
+	data = get_study_keys[study]
+	study_data = get_study(study)
+	if data.keys.include?('section_to_year')
+		# multi-year study
+		data['section_to_year'].each_pair do |section, year|
+			year_data = study_data.select do |hash|
+				hash[hash.keys[2]].to_i == section.to_i
+			end
+			ap year_data
+			File.open("#{DATA_PATH}/years/#{year.to_s}.json", 'w+') do |file|
+				file.write JSON.pretty_generate year_data
+			end
+		end
+	else
+		# single-year study
+		study_year = data['years'][0]
+		File.open("#{DATA_PATH}/years/#{study_year.to_s}.json", 'w+') do |file|
+			file.write JSON.pretty_generate study_data
+		end
+	end
+
+end
+
 if ARGV[0] == 'run'
 	run
 end
-
+if ARGV[0] == 'one'
+	one ARGV[1]
+end
