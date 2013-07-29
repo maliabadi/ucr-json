5ef3fbe51191e71a8aa00fc2ebd3e6fa37e267db
matt aliabadi <mattmaliabadi@gmail.com>
2013-07-29 03:18:39 -0700

    prettier ruby

--- lib/ucr-json/ucr-json.rb
+++ lib/ucr-json/ucr-json.rb
@@ -8,53 +8,53 @@ require 'mapper'
 require 'normalizer'
 
 module UCRJson
-	include UCRJson::Helpers
+  include UCRJson::Helpers
 
-	# various static paths
-	BASE_DIR = File.expand_path(File.dirname(__FILE__) + "/../../")
-	
-	# where various static JSON and TSV resources are kept
-	RELATION_PATH = "#{BASE_DIR}/relations"
+  # various static paths
+  BASE_DIR = File.expand_path(File.dirname(__FILE__) + "/../../")
 
-	# data at all stages of preparation
-	DATA_PATH = "#{BASE_DIR}/data"
-	
-	# text files from UCR
-	UNPARSED_PATH = "#{DATA_PATH}/unparsed"
-	
-	# Transitional, nominally parsed into JSON
-	PARSED_PATH = "#{DATA_PATH}/parsed"
-	
-	# Transitional, data mapped to years
-	YEAR_MAPPED_PATH = "#{DATA_PATH}/parsed/mapped"
+  # where various static JSON and TSV resources are kept
+  RELATION_PATH = "#{BASE_DIR}/relations"
 
-	# Final, data normalized and ready for use
-	NORMALIZED_PATH = "#{DATA_PATH}/parsed/normalized"
-	
+  # data at all stages of preparation
+  DATA_PATH = "#{BASE_DIR}/data"
 
-	def load!
-		parse_data!
-		map_data!
-		normalize_data!
-	end
+  # text files from UCR
+  UNPARSED_PATH = "#{DATA_PATH}/unparsed"
 
-	def parse_data!
-		Dir["#{UNPARSED_PATH}/*.txt"].each do |path|
-			Parser.run(path)
-		end
-	end
+  # Transitional, nominally parsed into JSON
+  PARSED_PATH = "#{DATA_PATH}/parsed"
 
-	def map_data!
-		Dir["#{PARSED_PATH}/*.json"].each do |path|
-			Mapper.run year_from_path(path)
-		end	
-	end
+  # Transitional, data mapped to years
+  YEAR_MAPPED_PATH = "#{DATA_PATH}/parsed/mapped"
+
+  # Final, data normalized and ready for use
+  NORMALIZED_PATH = "#{DATA_PATH}/parsed/normalized"
 
 
-	def normalize_data!
-		Dir["#{YEAR_MAPPED_PATH}/*.json"].each do |path|
-			Normalizer.run year_from_path(path)
-		end
-	end
+  def load!
+    parse_data!
+    map_data!
+    normalize_data!
+  end
+
+  def parse_data!
+    Dir["#{UNPARSED_PATH}/*.txt"].each do |path|
+      Parser.run(path)
+    end
+  end
+
+  def map_data!
+    Dir["#{PARSED_PATH}/*.json"].each do |path|
+      Mapper.run year_from_path(path)
+    end
+  end
+
+
+  def normalize_data!
+    Dir["#{YEAR_MAPPED_PATH}/*.json"].each do |path|
+      Normalizer.run year_from_path(path)
+    end
+  end
 
 end
